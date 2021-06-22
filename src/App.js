ec5eec0beb97204155d701d3024f3b449cfbb772
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2021-06-22 17:46:53 +0200

    useEffect multireturn with fetch()

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Setup from './tutorial/2-useEffect/setup/3-useEffect-fetch-data'
-import Final from './tutorial/2-useEffect/final/3-useEffect-fetch-data'
+import Setup from './tutorial/3-conditional-rendering/setup/1-multiple-returns'
+import Final from './tutorial/3-conditional-rendering/final/1-multiple-returns'
 
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced React Tutorial</h2>
+      <h2>Conditional rendering</h2>
       <br />
       <h3>Setup</h3>
       <hr />
--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,57 @@
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+
+  const [isLoading,setIsLoading] = useState(true);
+  const [isError,setIsError] = useState(false);
+  const [user,setUser] = useState('default user');
+  //const [respError,setRespError] = useState('Error....');
+
+  useEffect(()=>{
+    //setIsLoading(true);
+    fetch(url)
+      .then((resp)=>{
+        if(resp.status>=200 && resp.status<=299){
+          return resp.json();
+        }else{
+          setIsLoading(false);
+          setIsError(true);
+          //setRespError(`${resp.statusText}`);
+          //console.log(resp.statusText);
+          throw new Error(resp.statusText);
+        }
+      
+        
+      })  
+      .then( data => {
+        setIsLoading(false);
+        const {login} = data;
+        setUser(login);
+      })
+      .catch(error => console.log(error))
+  },[])
+
+  if(isLoading){
+      return (
+        <div>
+          <h2>Loading......</h2>
+        </div>
+      );
+  }
+
+  if(isError){
+    return (
+      <div>
+        <h2>Erorr....</h2>
+      </div>
+    );
+}
+
+  return (
+    <>
+      <h2>{user}</h2>
+    </>
+  );
 };
 
 export default MultipleReturns;
