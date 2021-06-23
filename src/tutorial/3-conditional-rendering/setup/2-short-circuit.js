f67d5911bb407c4cf287c341a1ebe447c1b7d996
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2021-06-23 10:22:14 +0200

    ternary operator

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -6,8 +6,11 @@ const ShortCircuit = () => {
   
   
   const [text, setText] = useState('');
+  const [isError, setIsError] = useState(false);
+
   const firstValue = text || 'hello world';
   const secondValue = text && 'hello world';
+
   console.log(secondValue);
   console.log(firstValue);
   return (
@@ -16,7 +19,9 @@ const ShortCircuit = () => {
       {/* <h2>firstValue: {firstValue}</h2> */}
       {/* <h2>secondValue: {secondValue}</h2> */}
       <h1>{text ||'john doe'}</h1>
-      {text && <h1>hello world</h1>}
+      <button className='btn' onClick={()=>setIsError(!isError)}>toggle error</button>
+      {isError && <h1>Error......</h1>}
+      <p>ternary expression-> {isError ? 'isError is true' : 'isError is false' }</p>
     </>
   )
 };
