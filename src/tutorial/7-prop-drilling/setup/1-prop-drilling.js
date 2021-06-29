162ab7e0c8ab4fb3845b7ed17f94919f1138972b
Yuanheng Li <foureverhh@Yuanhengs-Air.localdomain>
2021-06-29 18:47:20 +0200

    props drilling props passes from parent component to subcomponents

--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -1,10 +1,42 @@
 import React, { useState } from 'react';
-
+import {data} from '../../../data'
 // more components
 // fix - context api, redux (for more complex cases)
 
 const PropDrilling = () => {
-  return <h2>prop drilling</h2>;
+  const [people, setPeople] = useState(data);
+  
+  const removePerson = (id) => {
+    setPeople((prevPeople)=>{
+        return prevPeople.filter((person) => 
+          id !== person.id
+        )
+    });
+  }
+  return (
+    <>
+      <h2>prop drilling</h2>
+      <List people={people} removePerson={removePerson}/>
+    </>
+  );
 };
 
+const List = ({people,removePerson}) => {
+  return (
+    <ul>
+      {people.map((person)=>
+          <SinglePerson key={person.id} {...person} removePerson={removePerson}/>
+        )
+      }
+    </ul>
+  );
+}
+
+const SinglePerson = ({id,name,removePerson}) => {
+  return (
+    <li className="item">{name} 
+      <button className="btn" onClick={() =>removePerson(id)}>remove</button>
+    </li>
+  );
+}
 export default PropDrilling;
